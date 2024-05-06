ebcf4dda167c83d7701d17451fa3a1bf912b31a3
Luis Chaparro <luis.chaparro@estudiantat.upc.edu>
2024-05-06 18:44:03 +0200

    Escenas

---
+++ Assets/TransitionManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionManager : MonoBehaviour
+{
+    public FadeScreen fadeScreen;
+
+    public void GotoScene(int sceneIndex)
+    {
+        StartCoroutine(GoToSceneRoutine(sceneIndex));
+    }
+    IEnumerator GoToSceneRoutine(int sceneIndex)
+    {
+        fadeScreen.FadeOut();
+        yield return new WaitForSeconds(fadeScreen.fadeDuration);
+
+        //Abre la nueva escena
+        SceneManager.LoadScene(sceneIndex);
+
+    }
+
+    public void GotoSceneAsync(int sceneIndex)
+    {
+        StartCoroutine(GoToSceneAsyncRoutine(sceneIndex));
+    }
+    IEnumerator GoToSceneAsyncRoutine(int sceneIndex)
+    {
+        fadeScreen.FadeOut();      
+        //Abre la nueva escena
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        operation.allowSceneActivation = false;
+
+        float timer = 0;
+        while(timer <= fadeScreen.fadeDuration && !operation.isDone) 
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        operation.allowSceneActivation = true;
+
+    }
+}
