a19d9c1dfbfc8dbc485170f9ae89cebcc1024b90
Luis Chaparro <luis.chaparro@estudiantat.upc.edu>
2024-05-19 03:54:13 +0200

    Gaviotas

---
+++ Assets/WaypointMover.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointMover : MonoBehaviour
+{
+    [SerializeField] private Waypoints waypoints;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float distanceThreshold= 0.1f;
+
+    private Transform currentWaypoint;
+
+    void Start()
+    {
+        currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
+        transform.position = currentWaypoint.position;
+
+        currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
+        transform.LookAt(currentWaypoint);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, moveSpeed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, currentWaypoint.position) < distanceThreshold)
+        {
+            currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
+            transform.LookAt(currentWaypoint);
+        }
+    }
+}
